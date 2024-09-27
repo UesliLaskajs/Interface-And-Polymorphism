f8fdd8db7341ffe6e1babd5634d0b205daf7d113
UesliLaskajs <uesli.laska@gmail.com>
2024-09-27 17:47:07 +0200

    Learining about interface and polymorhism

---
+++ Payments.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface
+{
+    public interface IProcessPayments//Created an Interface for a void Method Payment Amount Passing
+    {
+        void PaymentProccessor(decimal amount);
+    }
+
+    public class Payments : IProcessPayments//Inerited From Interface
+    {
+        public void PaymentProccessor(decimal amount)//Used The inherited Method From interface
+        {
+            Console.WriteLine($"Payment Amount:{amount}$ processed");
+        }
+    }
+
+    public class CreditPayments : IProcessPayments
+    {
+        public void PaymentProccessor(decimal amount)
+        {
+            Console.WriteLine($"Credit Payment Amount:{amount}$ processed");
+        }
+    }
+
+    public class PaymentService//Created Another Class for Proccessing 2 classed above in the main method
+    {
+        private readonly IProcessPayments _processPayments;//private readonly field that stores the object proccesor that will be initialized in main Method
+
+        public PaymentService(IProcessPayments processor)//Constructor that calls the passed object from main method into the member field
+        {
+            _processPayments= processor;    
+        }
+
+        public void ProccesOrderPayment(decimal amount)//Class created 
+        {
+            _processPayments.PaymentProccessor(amount);//From the proccessor object created in the main method used the inherited PaymentProccesor of the Interface
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿namespace Interface
+{
+    internal class Program
+    {   
+        static void Main(string[] args)
+        {
+            IAnimal cat = new Cat();
+            cat.MakeSound();
+
+            IProcessPayments creditPayments = new CreditPayments();//Instantiated the credit Payment class from the Interface that will use later the method of interface
+            PaymentService paymentService = new PaymentService(creditPayments);//Instantiated the Class that will get the processor object creditPayment
+            paymentService.ProccesOrderPayment(20.00m);//Called The method of the object 
+
+            IProcessPayments bankPayment=new Payments();//Same for BankPayment Class
+            paymentService=new PaymentService(bankPayment);
+            paymentService.ProccesOrderPayment(100.00m);
+
+            Console.ReadKey();
+        }
+
+        public interface IAnimal
+        {
+            void MakeSound();
+            string Name(string name);
+        }
+
+
+        class Cat : IAnimal
+        {
+            void IAnimal.MakeSound()
+            {
+                Console.WriteLine("Meowwww");
+            }
+            
+             string IAnimal.Name(string name)
+            {
+                Console.WriteLine(name);
+                return name;
+            }
+        }
+
+    }
+}
